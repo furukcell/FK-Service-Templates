e257f4da6c5a7a0a441dd7a40e9c2bcbe4d25fec
furukcell <kurtulusfaruk94@gmail.com>
2026-07-09 19:17:37 +0300

    Add password reset helper

--- packages/firebase/src/auth.ts
+++ packages/firebase/src/auth.ts
@@ -1,8 +1,12 @@
-import { onAuthStateChanged, signInWithEmailAndPassword, signOut, type User } from "firebase/auth";
+import { onAuthStateChanged, sendPasswordResetEmail, signInWithEmailAndPassword, signOut, type User } from "firebase/auth";
 import { getFirebaseAuth } from "./client";
 
 export async function loginWithEmail(email: string, password: string) {
   return signInWithEmailAndPassword(getFirebaseAuth(), email, password);
+}
+
+export async function sendAdminPasswordReset(email: string) {
+  return sendPasswordResetEmail(getFirebaseAuth(), email);
 }
 
 export async function logoutCurrentUser() {
