7b1f06302ad786351ce9baaa938e73f579727e0a
furukcell <kurtulusfaruk94@gmail.com>
2026-07-09 16:29:42 +0300

    Use managed config on real estate page

--- apps/web/pages/real-estate.tsx
+++ apps/web/pages/real-estate.tsx
@@ -1,16 +1,19 @@
 import { TemplateLanding } from "../src/components/TemplateLanding";
 import { templateConfigs } from "../src/templateConfigs";
 import { useLayoutVariantFromQuery } from "../src/useLayoutVariantFromQuery";
+import { useManagedTemplateConfig } from "../src/useManagedTemplateConfig";
 
 export default function RealEstateTemplatePage() {
-  const activeLayout = useLayoutVariantFromQuery("modern");
+  const { config, layoutVariant } = useManagedTemplateConfig(templateConfigs["real-estate"]);
+  const activeLayout = useLayoutVariantFromQuery(layoutVariant || "modern");
 
   return (
     <TemplateLanding
-      config={templateConfigs["real-estate"]}
+      config={config}
       activeTemplate="real-estate"
       activeLayout={activeLayout}
       showTemplateSwitch={false}
+      showLayoutSwitch={false}
     />
   );
 }
