2f8b60beeb3f9f300fa7a7958e4974a6e2f1324e
furukcell <kurtulusfaruk94@gmail.com>
2026-07-09 19:19:35 +0300

    Add SEO head to real estate page

--- apps/web/pages/real-estate.tsx
+++ apps/web/pages/real-estate.tsx
@@ -1,3 +1,4 @@
+import { SeoHead } from "../src/components/SeoHead";
 import { TemplateLanding } from "../src/components/TemplateLanding";
 import { templateConfigs } from "../src/templateConfigs";
 import { useLayoutVariantFromQuery } from "../src/useLayoutVariantFromQuery";
@@ -8,12 +9,15 @@ export default function RealEstateTemplatePage() {
   const activeLayout = useLayoutVariantFromQuery(layoutVariant || "modern");
 
   return (
-    <TemplateLanding
-      config={config}
-      activeTemplate="real-estate"
-      activeLayout={activeLayout}
-      showTemplateSwitch={false}
-      showLayoutSwitch={false}
-    />
+    <>
+      <SeoHead title={`${config.brandName} | ${config.sector}`} description={config.heroDescription} canonicalPath="/real-estate" />
+      <TemplateLanding
+        config={config}
+        activeTemplate="real-estate"
+        activeLayout={activeLayout}
+        showTemplateSwitch={false}
+        showLayoutSwitch={false}
+      />
+    </>
   );
 }
