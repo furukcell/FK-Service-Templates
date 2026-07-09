6656437959d3a16e23458e26846f4425cd18a187
furukcell <kurtulusfaruk94@gmail.com>
2026-07-09 14:55:08 +0300

    Import admin dashboard styles

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import "../src/styles/globals.css";
+import "../src/styles/admin.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
