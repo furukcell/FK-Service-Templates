d4e81a035a7b8d332a7da779cd3e252358c21c75
furukcell <kurtulusfaruk94@gmail.com>
2026-07-09 16:17:09 +0300

    Support layout query on real estate page

--- apps/web/pages/real-estate.tsx
+++ apps/web/pages/real-estate.tsx
@@ -1,11 +1,15 @@
 import { TemplateLanding } from "../src/components/TemplateLanding";
 import { templateConfigs } from "../src/templateConfigs";
+import { useLayoutVariantFromQuery } from "../src/useLayoutVariantFromQuery";
 
 export default function RealEstateTemplatePage() {
+  const activeLayout = useLayoutVariantFromQuery("modern");
+
   return (
     <TemplateLanding
       config={templateConfigs["real-estate"]}
       activeTemplate="real-estate"
+      activeLayout={activeLayout}
       showTemplateSwitch={false}
     />
   );
