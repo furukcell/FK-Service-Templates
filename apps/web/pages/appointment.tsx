ded62c49cce040ebf609481582580a5f121b0c59
furukcell <kurtulusfaruk94@gmail.com>
2026-07-09 16:16:51 +0300

    Support layout query on appointment page

--- apps/web/pages/appointment.tsx
+++ apps/web/pages/appointment.tsx
@@ -1,11 +1,15 @@
 import { TemplateLanding } from "../src/components/TemplateLanding";
 import { templateConfigs } from "../src/templateConfigs";
+import { useLayoutVariantFromQuery } from "../src/useLayoutVariantFromQuery";
 
 export default function AppointmentTemplatePage() {
+  const activeLayout = useLayoutVariantFromQuery("modern");
+
   return (
     <TemplateLanding
       config={templateConfigs.appointment}
       activeTemplate="appointment"
+      activeLayout={activeLayout}
       showTemplateSwitch={false}
     />
   );
