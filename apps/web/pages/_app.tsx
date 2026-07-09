beb5b0f9063eee1bf7597e0199f94d0b7cea0447
furukcell <kurtulusfaruk94@gmail.com>
2026-07-09 15:03:18 +0300

    Import property styles

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -3,6 +3,7 @@ import "../src/styles/globals.css";
 import "../src/styles/admin.css";
 import "../src/styles/admin-extra.css";
 import "../src/styles/forms.css";
+import "../src/styles/properties.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
