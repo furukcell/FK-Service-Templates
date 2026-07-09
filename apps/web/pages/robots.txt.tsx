9f6040dd976cbb2579901ca71bc44724928fe0cb
furukcell <kurtulusfaruk94@gmail.com>
2026-07-09 21:19:48 +0300

    Disallow private admin routes in robots

--- apps/web/pages/robots.txt.tsx
+++ apps/web/pages/robots.txt.tsx
@@ -2,7 +2,16 @@ import type { GetServerSideProps } from "next";
 
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
   const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://example.com";
-  const robots = `User-agent: *\nAllow: /\nSitemap: ${siteUrl}/sitemap.xml\n`;
+  const robots = [
+    "User-agent: *",
+    "Allow: /",
+    "Disallow: /admin",
+    "Disallow: /login",
+    "Disallow: /forgot-password",
+    "Disallow: /api/",
+    `Sitemap: ${siteUrl}/sitemap.xml`,
+    ""
+  ].join("\n");
   res.setHeader("Content-Type", "text/plain");
   res.write(robots);
   res.end();
