2ab83ba2fc18279613c4f1385085e2b4d2e45aff
furukcell <kurtulusfaruk94@gmail.com>
2026-07-09 16:29:35 +0300

    Use managed config on salon page

--- apps/web/pages/salon.tsx
+++ apps/web/pages/salon.tsx
@@ -1,16 +1,19 @@
 import { TemplateLanding } from "../src/components/TemplateLanding";
 import { templateConfigs } from "../src/templateConfigs";
 import { useLayoutVariantFromQuery } from "../src/useLayoutVariantFromQuery";
+import { useManagedTemplateConfig } from "../src/useManagedTemplateConfig";
 
 export default function SalonTemplatePage() {
-  const activeLayout = useLayoutVariantFromQuery("modern");
+  const { config, layoutVariant } = useManagedTemplateConfig(templateConfigs.salon);
+  const activeLayout = useLayoutVariantFromQuery(layoutVariant || "modern");
 
   return (
     <TemplateLanding
-      config={templateConfigs.salon}
+      config={config}
       activeTemplate="salon"
       activeLayout={activeLayout}
       showTemplateSwitch={false}
+      showLayoutSwitch={false}
     />
   );
 }
