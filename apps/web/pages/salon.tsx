88a36430833b16d70b6569d5e4cedca0cc68af99
furukcell <kurtulusfaruk94@gmail.com>
2026-07-09 16:17:01 +0300

    Support layout query on salon page

--- apps/web/pages/salon.tsx
+++ apps/web/pages/salon.tsx
@@ -1,11 +1,15 @@
 import { TemplateLanding } from "../src/components/TemplateLanding";
 import { templateConfigs } from "../src/templateConfigs";
+import { useLayoutVariantFromQuery } from "../src/useLayoutVariantFromQuery";
 
 export default function SalonTemplatePage() {
+  const activeLayout = useLayoutVariantFromQuery("modern");
+
   return (
     <TemplateLanding
       config={templateConfigs.salon}
       activeTemplate="salon"
+      activeLayout={activeLayout}
       showTemplateSwitch={false}
     />
   );
