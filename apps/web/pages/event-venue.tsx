510e71647984660001ce94ddc679e5edcbab85cc
furukcell <kurtulusfaruk94@gmail.com>
2026-07-10 14:09:24 +0300

    Add event venue page

---
+++ apps/web/pages/event-venue.tsx
@@ -0,0 +1,26 @@
+import { SeoHead } from "../src/components/SeoHead";
+import { SiteSetupGuard } from "../src/components/SiteSetupGuard";
+import { TemplateLanding } from "../src/components/TemplateLanding";
+import { templateConfigs } from "../src/templateConfigs";
+import { useLayoutVariantFromQuery } from "../src/useLayoutVariantFromQuery";
+import { useManagedTemplateConfig } from "../src/useManagedTemplateConfig";
+
+export default function EventVenueTemplatePage() {
+  const { config, layoutVariant, requiresSetup } = useManagedTemplateConfig(templateConfigs["event-venue"]);
+  const activeLayout = useLayoutVariantFromQuery(layoutVariant || "modern");
+
+  if (requiresSetup) return <SiteSetupGuard />;
+
+  return (
+    <>
+      <SeoHead title={`${config.brandName} | ${config.sector}`} description={config.heroDescription} canonicalPath="/event-venue" />
+      <TemplateLanding
+        config={config}
+        activeTemplate="event-venue"
+        activeLayout={activeLayout}
+        showTemplateSwitch={false}
+        showLayoutSwitch={false}
+      />
+    </>
+  );
+}
