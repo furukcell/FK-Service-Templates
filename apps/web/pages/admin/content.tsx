f55964e0149f96b79af83c4fc852246f666f11bf
furukcell <kurtulusfaruk94@gmail.com>
2026-07-10 08:13:11 +0300

    Include cafe template in content admin

--- apps/web/pages/admin/content.tsx
+++ apps/web/pages/admin/content.tsx
@@ -5,7 +5,7 @@ import { contentPageLabels, contentPageOrder, defaultContentPages, defaultFaqIte
 import { templateConfigs } from "../../src/templateConfigs";
 import { useOptionalAdminGuard } from "../../src/useOptionalAdminGuard";
 
-const templateKeys: TemplateKey[] = ["appointment", "salon", "real-estate"];
+const templateKeys: TemplateKey[] = ["appointment", "salon", "real-estate", "cafe"];
 
 export default function AdminContentPage() {
   const guard = useOptionalAdminGuard();
