dd490b46c01b677e71f8b4473ec883076d081206
furukcell <kurtulusfaruk94@gmail.com>
2026-07-13 10:14:35 +0300

    Add Lotus Borek demo page

---
+++ apps/web/pages/lotus-borek-evi.tsx
@@ -0,0 +1,23 @@
+import { demoLotusBorekConfig } from "../../../configs/demo-lotus-borek";
+import { SeoHead } from "../src/components/SeoHead";
+import { TemplateLanding } from "../src/components/TemplateLanding";
+
+export default function LotusBorekDemoPage() {
+  return (
+    <>
+      <SeoHead
+        title="Lotus Börek Evi | Günlük Taze Börek ve Tepsi Sipariş"
+        description={demoLotusBorekConfig.heroDescription}
+        canonicalPath="/lotus-borek-evi"
+        noIndex
+      />
+      <TemplateLanding
+        config={demoLotusBorekConfig}
+        activeTemplate="cafe"
+        activeLayout="showcase"
+        showTemplateSwitch={false}
+        showLayoutSwitch={false}
+      />
+    </>
+  );
+}
