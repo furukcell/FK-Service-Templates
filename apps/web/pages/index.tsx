7a9d7065ab1d1211fa593a096d14d48a536e99e2
furukcell <kurtulusfaruk94@gmail.com>
2026-07-13 09:52:20 +0300

    Use default template on home demo page

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -3,11 +3,12 @@ import type { LayoutVariant, TemplateKey } from "@fk-templates/shared";
 import { SeoHead } from "../src/components/SeoHead";
 import { SiteSetupGuard } from "../src/components/SiteSetupGuard";
 import { TemplateLanding } from "../src/components/TemplateLanding";
+import { getDefaultTemplate } from "../src/defaultTemplate";
 import { templateConfigs } from "../src/templateConfigs";
 import { useManagedTemplateConfig } from "../src/useManagedTemplateConfig";
 
 export default function HomePage() {
-  const [activeTemplate, setActiveTemplate] = useState<TemplateKey>("appointment");
+  const [activeTemplate, setActiveTemplate] = useState<TemplateKey>(getDefaultTemplate());
   const [activeLayout, setActiveLayout] = useState<LayoutVariant>("modern");
   const baseConfig = templateConfigs[activeTemplate];
   const { config, requiresSetup } = useManagedTemplateConfig(baseConfig);
