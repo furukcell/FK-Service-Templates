7367c28bd384401548d3813ae01adf0c64f7412a
furukcell <kurtulusfaruk94@gmail.com>
2026-07-14 13:19:34 +0300

    Load salon booking styles

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -11,6 +11,7 @@ import "../src/styles/visual-sections.css";
 import "../src/styles/layout-variants.css";
 import "../src/styles/content-pages.css";
 import "../src/styles/floating-whatsapp.css";
+import "../src/styles/salon-booking.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <><Component {...pageProps} /><CookieBanner /></>;
