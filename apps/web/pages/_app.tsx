d3dcde8d4eb2e39db16337ba3e08b0be885b8add
furukcell <kurtulusfaruk94@gmail.com>
2026-07-14 16:07:41 +0300

    Load mobile flow deck carousel styles

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -26,6 +26,7 @@ import "../src/styles/salon-final-tweaks.css";
 import "../src/styles/salon-gallery-premium.css";
 import "../src/styles/flow-all-sectors.css";
 import "../src/styles/flow-mobile-refine.css";
+import "../src/styles/flow-mobile-deck.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <><Component {...pageProps} /><SalonAdminNavigation /><CookieBanner /></>;
