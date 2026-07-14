6c91b87be47eb6b74de3230d5b3b7cfc586c9ee1
furukcell <kurtulusfaruk94@gmail.com>
2026-07-14 14:43:36 +0300

    Load premium salon card styles

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -19,6 +19,7 @@ import "../src/styles/salon-flow-preview.css";
 import "../src/styles/salon-site-enhancements.css";
 import "../src/styles/salon-hero-slider.css";
 import "../src/styles/salon-hero-integrated.css";
+import "../src/styles/salon-premium-cards.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <><Component {...pageProps} /><SalonAdminNavigation /><CookieBanner /></>;
