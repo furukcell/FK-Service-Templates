db0efc0fb865a56f974d2267a7868e7cc3dc65ff
furukcell <kurtulusfaruk94@gmail.com>
2026-07-15 14:02:47 +0300

    Load premium salon carousel styles

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -31,6 +31,7 @@ import "../src/styles/flow-mobile-deck.css";
 import "../src/styles/flow-mobile-final.css";
 // Approved salon header and hero use unique classes and intentionally load last.
 import "../src/styles/salon-premium-approved.css";
+import "../src/styles/salon-premium-carousel.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <><Component {...pageProps} /><SalonAdminNavigation /><CookieBanner /></>;
