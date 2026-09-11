d309a9f491bc2f2000bab95bff9d0ab325f83623
furukcell <kurtulusfaruk94@gmail.com>
2026-09-11 16:24:23 +0300

    Load final kindergarten bridge and card motion refinements

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -53,6 +53,7 @@ import "../src/styles/kindergarten-hero-clean.css";
 import "../src/styles/kindergarten-hero-final.css";
 import "../src/styles/kindergarten-story-final.css";
 import "../src/styles/kindergarten-science-doodles.css";
+import "../src/styles/kindergarten-why-bridge-fix.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <><Component {...pageProps} /><SalonAdminNavigation /><CookieBanner /></>;
