c1c2938748dbbf717c1d8582dec509c91b093b8f
furukcell <kurtulusfaruk94@gmail.com>
2026-09-11 10:49:29 +0300

    Load final kindergarten story animation styles

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -51,6 +51,7 @@ import "../src/styles/kindergarten-reference-overlap.css";
 import "../src/styles/kindergarten-hero-motion.css";
 import "../src/styles/kindergarten-hero-clean.css";
 import "../src/styles/kindergarten-hero-final.css";
+import "../src/styles/kindergarten-story-final.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <><Component {...pageProps} /><SalonAdminNavigation /><CookieBanner /></>;
