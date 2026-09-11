6cfb7b9b132c2a43abedc1103d0463497464082f
furukcell <kurtulusfaruk94@gmail.com>
2026-09-11 08:33:20 +0300

    Load kindergarten hero overlap refinement

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -47,6 +47,7 @@ import "../src/styles/kindergarten-blue-enhancements.css";
 import "../src/styles/kindergarten-design-chooser.css";
 import "../src/styles/kindergarten-reference-fidelity.css";
 import "../src/styles/kindergarten-wave-sections.css";
+import "../src/styles/kindergarten-reference-overlap.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <><Component {...pageProps} /><SalonAdminNavigation /><CookieBanner /></>;
